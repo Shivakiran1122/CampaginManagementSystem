c60527d3f32527199b567437306b6ab5e3a7b900
Shivakiran1122 <125103363+Shivakiran1122@users.noreply.github.com>
2023-02-14 19:05:49 -0600

    Delete test1/src directory

--- test1/src/main/java/com/example/demo/repository/TempImageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.entity.TempImages;
-
-public interface TempImageRepository extends JpaRepository<TempImages,Integer> {
-
-}
